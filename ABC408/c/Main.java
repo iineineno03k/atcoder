9a803a0a9e2875265db649719928c23f75ca3d08
iineineno03k <iineineno03k@gmail.com>
2025-05-31 20:36:14 +0900

    雛形作成

---
+++ ABC408/c/Main.java
@@ -0,0 +1,13 @@
+package c;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // TODO: 問題に応じて実装
+        
+        sc.close();
+    }
+}
