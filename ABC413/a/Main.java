4a31261e7e7d8038277fceaa0135babc6022c8c8
iineineno03k <iineineno03k@gmail.com>
2025-07-05 20:57:53 +0900

    create 413

---
+++ ABC413/a/Main.java
@@ -0,0 +1,17 @@
+package ABC413.a;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 入力処理
+
+        // 処理・計算
+
+        // 出力
+
+        sc.close();
+    }
+}
---
+++ ABC413/e/Main.java
@@ -0,0 +1,20 @@
+package ABC413.e;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 入力処理
+        
+
+        // 処理・計算
+        
+
+        // 出力
+        
+
+        sc.close();
+    }
+}
